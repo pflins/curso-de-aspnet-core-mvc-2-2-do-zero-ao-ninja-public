6d51e3b119e6a41c00660edca6f92264db24d6c9
Paulo Lins <paulo.lins@pflins.com>
2022-03-12 10:08:53 +0000

    Ainda funcionando a criação da View pelo Scaffolding após instalado o Microsoft.EntityFrameworkCore.SqlServer no projeto Data. Obs: Nesta instalação, o NuGet não instalou o Tools nem o Design.

--- src/Cooperchip.ITDeveloper.Data/ORM/ITDeveloperDbContext.cs
+++ src/Cooperchip.ITDeveloper.Data/ORM/ITDeveloperDbContext.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Cooperchip.ITDeveloper.Data.ORM
 {
-    public class ITDeveloperDbContext
+    public class ITDeveloperDbContext : DbContext
     {
     }
 }
