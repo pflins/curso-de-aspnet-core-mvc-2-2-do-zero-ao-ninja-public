fcc813fa96f2fe80fa9fb6958c09aac863b08bf7
Paulo Lins <paulo.lins@pflins.com>
2022-03-15 07:15:48 +0000

    Seção 5: ViewModel e DataAnnotations.

---
+++ src/Cooperchip.ITDeveloper.Domain/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cooperchip.ITDeveloper.Domain.Entities
+{
+    public class EntityBase
+    {
+        public EntityBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+    }
+}
