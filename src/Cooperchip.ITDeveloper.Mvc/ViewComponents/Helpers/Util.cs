472c08c2dfd3122bc5706829401b39cf1bfda342
Paulo Lins <paulo.lins@pflins.com>
2022-03-24 07:31:41 +0000

    Finalizado a Seção 7: ViewComponents. A criação da View pelo Scaffolding ainda não funciona.

---
+++ src/Cooperchip.ITDeveloper.Mvc/ViewComponents/Helpers/Util.cs
@@ -0,0 +1,27 @@
+﻿using Cooperchip.ITDeveloper.Data.ORM;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Cooperchip.ITDeveloper.Mvc.Extentions.ViewComponents.Helpers
+{
+    public static class Util
+    {
+        public static int TotReg(ITDeveloperDbContext ctx)
+        {
+            var paciente = from pac in ctx.Paciente.AsNoTracking() select pac;
+            //return (from pac in ctx.Paciente.AsNoTracking() select pac).Count();
+            if (paciente.Any())
+            {
+                return paciente.Count();
+            }
+            return 1;
+        }
+
+        public static decimal GetNumRegEstado(ITDeveloperDbContext ctx, string estado)
+        {
+            return ctx.Paciente.AsNoTracking()
+                .Count(x => x.EstadoPaciente.Descricao.Contains(estado));
+        }
+
+    }
+}
